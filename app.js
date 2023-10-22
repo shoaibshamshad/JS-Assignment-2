bdcd4e3cc89e857a2ab4fbbefc08e9bd1f634ef5
sufyan3 <sufyan@diyainteractive.com>
2023-10-22 13:06:40 +0500

    create variables

---
+++ app.js
@@ -0,0 +1,80 @@
+//var num = 5;
+//num = num + 1;
+//console.log(num)
+// 1. Write a program that take two numbers & add them in a
+//new variable. Show the result in your browser.
+//var num1 = +prompt("Enter num 1")
+//var num2 = +prompt("Enter num 2")
+
+//var total = num1 + num2;
+//console.log(total)
+
+// var num1 = +prompt("Enter num 1")
+// var num2 = +prompt("Enter num 2")
+
+// var total = num1 * num2;
+// console.log(total)
+
+
+// var num1 = +prompt("Enter num 1")
+// var num2 = +prompt("Enter num 2")
+
+// var total = num1 / num2;
+// console.log(total)
+
+
+// var num1 = +prompt("Enter num 1")
+// var num2 = +prompt("Enter num 2")
+
+// var total = num1 / num2;
+// console.log(total)
+
+
+// var num1 = +prompt("Enter num 1")
+// var num2 = +prompt("Enter num 2")
+
+// var total = num1 % num2;
+// console.log(total)
+
+// var value;
+// console.log( "value after variable declaration is: " + value)
+// value = 5;
+// console.log("Initial value: " + value)
+
+// var value = 6;
+// value++
+// console.log("Value after increment is: " + value)
+
+// var value = 7;
+// value = value + 6;
+// console.log("value after addition is: " +value)
+
+// var value;
+// console.log("Value after decrement is: " + value)
+// value = 13;
+// value--
+// console.log("Value after decrement is: " + value)
+
+// var value = 12;
+// value = value / 4;
+// console.log("the remainder after dividing the variable’s value " +value)
+// console.log("the remainder is: "+ 3)
+
+// Q 4. Cost of one movie ticket is 600 PKR. Write a script to
+// store
+// ticket price in a variable & calculate the cost of buying 5
+// tickets
+// to a movie. Example output:
+// total cost to buy 5 tickets to a movie is 3000pkr.
+
+// var ticket = 600;
+// ticket = ticket * 5;
+// console.log("total cost to buy 5 tickets to a movie is :" + ticket)
+
+// Q Write a script to display multiplication table of any
+// number in your browser. E.g
+
+var table = +prompt("please write a number");
+var st =
+    table + "x 1 " + "=" + table * 1 + "<br>" + table + "x 2 " + "=" + table * 2 + "<br>" + table + "x 3 " + "=" + table * 3 + "<br>" + table + "x 4 " + "=" + table * 4 + "<br>" + table + "x 5 " + "=" + table * 5 + "<br>" + table + "x 6 " + "=" + table * 6 + "<br>" + table + "x 7 " + "=" + table * 7 + "<br>" + table + "x 8 " + "=" + table * 8 + "<br>" + table + "x 9 " + "=" + table * 9 + "<br>" + table + "x 10 " + "=" + table * 10 + "<br>"
+document.write(st)
