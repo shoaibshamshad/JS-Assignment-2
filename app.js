d096986f7a7f63cf328c22618532436819a8a232
sufyan3 <sufyan@diyainteractive.com>
2023-11-18 23:42:42 +0500

    update javascript questions

--- app.js
+++ app.js
@@ -71,10 +71,262 @@
 // ticket = ticket * 5;
 // console.log("total cost to buy 5 tickets to a movie is :" + ticket)
 
-// Q Write a script to display multiplication table of any
+// Q Write a script to display multiplication table NumInpof any
 // number in your browser. E.g
 
-var table = +prompt("please write a number");
-var st =
-    table + "x 1 " + "=" + table * 1 + "<br>" + table + "x 2 " + "=" + table * 2 + "<br>" + table + "x 3 " + "=" + table * 3 + "<br>" + table + "x 4 " + "=" + table * 4 + "<br>" + table + "x 5 " + "=" + table * 5 + "<br>" + table + "x 6 " + "=" + table * 6 + "<br>" + table + "x 7 " + "=" + table * 7 + "<br>" + table + "x 8 " + "=" + table * 8 + "<br>" + table + "x 9 " + "=" + table * 9 + "<br>" + table + "x 10 " + "=" + table * 10 + "<br>"
-document.write(st)
+// var tableNumInp = +prompt("please write a number");
+// var st =
+//     tableNumInp + "x 1 " + "=" + tableNumInp * 1 + "<br>"
+//    + tableNumInp + "x 2 " + "=" + tableNumInp * 2 + "<br>"
+//    + tableNumInp + "x 3 " + "=" + tableNumInp * 3 + "<br>"
+//    + tableNumInp + "x 4 " + "=" + tableNumInp * 4 + "<br>"
+//    + tableNumInp + "x 5 " + "=" + tableNumInp * 5 + "<br>"
+//    + tableNumInp + "x 6 " + "=" + tableNumInp * 6 + "<br>"
+//    + tableNumInp + "x 7 " + "=" + tableNumInp * 7 + "<br>"
+//    + tableNumInp + "x 8 " + "=" + tableNumInp * 8 + "<br>"
+//    + tableNumInp + "x 9 " + "=" + tableNumInp * 9 + "<br>"
+//    + tableNumInp + "x 10 " + "=" + tableNumInp * 10 + "<br>"
+// document.write(st)
+
+// 6. The Temperature Converter: It’s hot out! Let’s make a
+// converter based on the steps here.
+// a. Store a Celsius temperature into a variable.
+// b. Convert it to Fahrenheit & output “NNoC is NNoF”.
+// c. Now store a Fahrenheit temperature into a variable.
+// d. Convert it to Celsius & output “NNoF is NNoC”.
+
+// var c = 25;
+// var f = 70;
+// var total = (f-32)*5/9;
+// document.write("70F is "+total)
+// var total = (c*9/5)+32;
+// document.write("25C is "+total)
+
+// 7. Write a program to implement checkout process of a
+// shopping cart system for an e-commerce website. Store
+// the following in variables
+// a. Price of item 1
+// b. Price of item 2
+// c. Ordered quantity of item 1
+// d. Ordered Quantity of item 2
+// e. Shipping charges
+
+// Compute the total cost & show the receipt in your browser.
+
+// var item1 = 650;
+// var item2 = 100;
+// var shippingCharges = 100;
+// var totalCost = (item1*3)+(item2*7)+shippingCharges;
+// document.write("total cost of your order is "+totalCost)
+
+// 8. Store total marks & marks obtained by a student in 2
+// variables. Compute the percentage & show the result in
+// your browser
+
+// var totalMarks = 980;
+// var obtainedMarks = 804;
+// var percentage = (obtainedMarks/totalMarks)*100;
+// document.write(percentage)
+
+
+// 9. Assume we have 10 US dollars & 25 Saudi Riyals. Write a
+// script to convert the total currency to Pakistani Rupees.
+// Perform all calculations in a single expression.
+// (Exchange rates : 1 US Dollar = 104.80 Pakistani Rupee
+// and 1 Saudi Riyal = 28 Pakistani Rupee)
+
+// var usDollar = 104.80;
+// var numOfDollar = 10;
+// var saudiRiyal = 28;
+// var numOsSaudiRiyal =25;
+// var totalCurrencyPkr = (usDollar*numOfDollar)+(saudiRiyal*numOsSaudiRiyal);
+// document.write("total currency of PKR : "+totalCurrencyPkr)
+
+
+// 10. Write a program to initialize a variable with some
+// number and do arithmetic in following sequence:
+// a. Add 5
+// b. Multiply by 10
+// c. Divide the result by 2
+// Perform all calculations in a single expression
+
+// var num1 = 4;
+// var total = (num1+5*(10)/2);
+// document.write(total)
+
+
+// 11. The Age Calculator: Forgot how old someone is?
+// Calculate it!
+// a. Store the current year in a variable.
+// b. Store their birth year in a variable.
+// c. Calculate their 2 possible ages based on the stored
+// values.
+
+// Output them to the screen like so: “They are either NN or NN
+// years old”.
+
+// var currentYear = 2016;
+// var birthYear = 1992;
+// var total = (currentYear-birthYear)
+// document.write("they are "+total + "years old")
+
+
+// 1. Write a program to take a number in a variable, do the
+// required arithmetic to display the following result in your
+// browser:
+
+// var a = 10;
+// document.write("the value of a is 10"+"</br></br>")
+// ++a
+// document.write("the value of ++a is "+a+"</br>")
+// document.write("now the value of a is: "+a+"</br></br>")
+// document.write("the value of a++ is:"+a+"</br>")
+// a++
+// document.write("now the value of a is: "+a+"</br></br>")
+// --a
+// document.write("the value of --a is: "+a+"</br>")
+// document.write("now the value of a is: "+a+"</br></br>")
+// document.write("the value of a-- is: "+a+"</br>")
+// a--
+// document.write("now the value of a is: "+a)
+
+
+// 2. What will be the output in variables a, b & result after
+// execution of the following script:
+
+// var a = 2;
+// var b = 1;
+// --a
+// document.write("a is: "+a+"</br>")
+// --b
+// document.write("b is: "+b+"</br>")
+// var total =
+// --a - --b + ++b + b--;
+// document.write("result is: "+total)
+
+
+// 3. Write a program that takes input a name from user &
+// greet the user.
+
+// var a = prompt("write your name")
+// alert("welcome "+a)
+
+
+// 5. Write a program to take input a number from user &
+// display it’s multiplication table on your browser. If user
+// does not enter a new number, multiplication table of 5
+// should be displayed by default.
+
+
+// var tableNumInp = +prompt("please write a number");
+// var st =
+//     tableNumInp + "x 1 " + "=" + tableNumInp * 1 + "<br>"
+//    + tableNumInp + "x 2 " + "=" + tableNumInp * 2 + "<br>"
+//    + tableNumInp + "x 3 " + "=" + tableNumInp * 3 + "<br>"
+//    + tableNumInp + "x 4 " + "=" + tableNumInp * 4 + "<br>"
+//    + tableNumInp + "x 5 " + "=" + tableNumInp * 5 + "<br>"
+//    + tableNumInp + "x 6 " + "=" + tableNumInp * 6 + "<br>"
+//    + tableNumInp + "x 7 " + "=" + tableNumInp * 7 + "<br>"
+//    + tableNumInp + "x 8 " + "=" + tableNumInp * 8 + "<br>"
+//    + tableNumInp + "x 9 " + "=" + tableNumInp * 9 + "<br>"
+//    + tableNumInp + "x 10 " + "=" + tableNumInp * 10 + "<br>"
+// document.write(st)
+
+
+// var math = 100;
+// var science = 100;
+// var english = 100;
+// var totalMarks = (math+science+english)
+// var mathObtained = 54;
+// var scienceObtained = 54;
+// var englishObtained = 48;
+// var totalObtainedMarks = (mathObtained+englishObtained+scienceObtained)
+// var totalMarks = 100;
+// var totalSubjectMarks = 300;
+// var totalObtainedMarks = 156;
+// var html = `
+// <table>
+//     <tr>
+// <th>Subject</th>
+// <th>Total Marks</th>
+// <th>Obtained Marks</th>
+// <th>Percentage</th>
+// </tr>
+// <tr>
+//     <td>English</td>
+//     <td>${totalMarks}</td>
+//     <td>${englishObtained}</td>
+//     <td>${englishObtained/totalMarks*100}%</td>
+// </tr>
+// <tr>
+//     <td>Math</td>
+//     <td>${totalMarks}</td>
+//     <td>${mathObtained}</td>
+//     <td>${mathObtained/totalMarks*100}%</td>
+// </tr>
+// <tr>
+//     <td>Science</td>
+//     <td>${totalMarks}</td>
+//     <td>${scienceObtained}</td>
+//     <td>${scienceObtained/totalMarks*100}%</td>
+// </tr>
+// <tr>
+//     <th></th>
+//     <th>${math+english+science}</th>
+//     <th>${mathObtained+englishObtained+scienceObtained}</th>
+//     <th>${totalObtainedMarks/totalSubjectMarks*100}%</th>
+// </tr>
+
+// </table>`
+// document.write(html)
+
+// USER INPUT AND CONDITIONAL STATEMENT
+
+// Q1
+// var city = prompt("enter your city name");
+// if (city === "karachi") {
+//     alert("well come to city of light");
+// }
+
+
+// Q2
+// var gender = prompt("type your gender");
+// if (gender === "male") {
+//     alert("Good Morning Sir");
+// }
+// else if (gender === "female") {
+//     alert("Good Morning Ma'am");
+// }
+
+
+// Q3
+// var signalColor = prompt("color of road traffic signals")
+// if (signalColor === "red"){
+//     alert("Must Stop")
+// }
+// else if (signalColor === "yellow"){
+//     alert("Ready to move")
+// }
+// else if (signalColor === "green"){
+//     alert("Move Now")
+// }
+
+
+// Q4
+// var fuel = prompt("type the current fuel in your car")
+// if (fuel < 0.25){
+//     alert("Please refill the fuel in your car")
+// }
+
+
+// Q5
+var a = 4;
+if (++a === 5){
+alert("given condition for variable a is true");
+}
+
+// Q5(b)
+var b = 82;
+if (b++ === 83) {
+    alert("given condition variable b is true");
+}
